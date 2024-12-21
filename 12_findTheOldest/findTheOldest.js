a95208a957418afb8629d78170e7628167a92fff
Devin Xie <devinxie418@gmail.com>
2024-12-20 22:03:07 -0500

    solved findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+  let age;
+  if("yearOfDeath" in person) {
+    const death = person.yearOfDeath;
+    const birth = person.yearOfBirth;
+    age = death-birth;
+  }
+  else {
+    const year = new Date().getFullYear();
+    const birth = person.yearOfBirth;
+    age = year-birth;
+  }
 
+  return age;
+}
+
+const findTheOldest = function(people) {
+  return people.reduce((acc, curr) => {
+    const maxAge = getAge(acc);
+    const currAge = getAge(curr);
+    return maxAge > currAge ? acc : curr;
+  });
 };
 
 // Do not edit below this line
